eeaa53cf9140b2feceea64635cd547bc2751ca9a
Filippo Projetto <fili.projetto@gmail.com>
2021-07-21 16:43:06 +0100

    add custom sections from liqo project
    - Add custom provider error handling logic: custom provider error handling to avoid pods to be marked as "provider failed" in some scenarios.
    - Ignore service env variables when creating pods: probably because the service IP in remote cluster is going to be different. Liqo deals with it here: https://github.com/liqotech/liqo/blob/4b8295046f2148418f86f3c987bafc292e7db7e9/pkg/virtualKubelet/translation/serviceEnv/serviceEnv.go#L14

--- node/env.go
+++ node/env.go
@@ -22,14 +22,10 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
 	apivalidation "k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/client-go/tools/record"
 	podshelper "k8s.io/kubernetes/pkg/apis/core/pods"
-	v1helper "k8s.io/kubernetes/pkg/apis/core/v1/helper"
 	fieldpath "k8s.io/kubernetes/pkg/fieldpath"
-	"k8s.io/kubernetes/pkg/kubelet/envvars"
 	"k8s.io/kubernetes/third_party/forked/golang/expansion"
 
 	"github.com/virtual-kubelet/virtual-kubelet/internal/manager"
@@ -68,8 +64,6 @@ const (
 	// ReasonInvalidEnvironmentVariableNames is the reason used in events emitted when a configmap/secret referenced in a ".spec.containers[*].envFrom" field contains invalid environment variable names.
 	ReasonInvalidEnvironmentVariableNames = "InvalidEnvironmentVariableNames"
 )
-
-var masterServices = sets.NewString("kubernetes")
 
 // populateEnvironmentVariables populates the environment of each container (and init container) in the specified pod.
 // TODO Make this the single exported function of a "pkg/environment" package in the future.
@@ -120,53 +114,6 @@ func populateContainerEnvironment(ctx context.Context, pod *corev1.Pod, containe
 	container.Env = res
 
 	return nil
-}
-
-// getServiceEnvVarMap makes a map[string]string of env vars for services a
-// pod in namespace ns should see.
-// Based on getServiceEnvVarMap in kubelet_pods.go.
-func getServiceEnvVarMap(rm *manager.ResourceManager, ns string, enableServiceLinks bool) (map[string]string, error) {
-	var (
-		serviceMap = make(map[string]*corev1.Service)
-		m          = make(map[string]string)
-	)
-
-	services, err := rm.ListServices()
-	if err != nil {
-		return nil, err
-	}
-
-	// project the services in namespace ns onto the master services
-	for i := range services {
-		service := services[i]
-		// ignore services where ClusterIP is "None" or empty
-		if !v1helper.IsServiceIPSet(service) {
-			continue
-		}
-		serviceName := service.Name
-
-		// We always want to add environment variables for master kubernetes service
-		// from the default namespace, even if enableServiceLinks is false.
-		// We also add environment variables for other services in the same
-		// namespace, if enableServiceLinks is true.
-		if service.Namespace == metav1.NamespaceDefault && masterServices.Has(serviceName) {
-			if _, exists := serviceMap[serviceName]; !exists {
-				serviceMap[serviceName] = service
-			}
-		} else if service.Namespace == ns && enableServiceLinks {
-			serviceMap[serviceName] = service
-		}
-	}
-
-	mappedServices := make([]*corev1.Service, 0, len(serviceMap))
-	for key := range serviceMap {
-		mappedServices = append(mappedServices, serviceMap[key])
-	}
-
-	for _, e := range envvars.FromServices(mappedServices) {
-		m[e.Name] = e.Value
-	}
-	return m, nil
 }
 
 // makeEnvironmentMapBasedOnEnvFrom returns a map representing the resolved environment of the specified container after being populated from the entries in the ".envFrom" field.
@@ -294,20 +241,7 @@ loop:
 // makeEnvironmentMap returns a map representing the resolved environment of the specified container after being populated from the entries in the ".env" and ".envFrom" field.
 func makeEnvironmentMap(ctx context.Context, pod *corev1.Pod, container *corev1.Container, rm *manager.ResourceManager, recorder record.EventRecorder, res map[string]string) error {
 
-	// TODO If pod.Spec.EnableServiceLinks is nil then fail as per 1.14 kubelet.
-	enableServiceLinks := corev1.DefaultEnableServiceLinks
-	if pod.Spec.EnableServiceLinks != nil {
-		enableServiceLinks = *pod.Spec.EnableServiceLinks
-	}
-
-	// Note that there is a race between Kubelet seeing the pod and kubelet seeing the service.
-	// To avoid this users can: (1) wait between starting a service and starting; or (2) detect
-	// missing service env var and exit and be restarted; or (3) use DNS instead of env vars
-	// and keep trying to resolve the DNS name of the service (recommended).
-	svcEnv, err := getServiceEnvVarMap(rm, pod.Namespace, enableServiceLinks)
-	if err != nil {
-		return err
-	}
+	svcEnv := map[string]string{}
 
 	// If the variable's Value is set, expand the `$(var)` references to other
 	// variables in the .Value field; the sources of variables are the declared
